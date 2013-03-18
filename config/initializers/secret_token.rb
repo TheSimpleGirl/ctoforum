0ea9ca858e6781bf5cbacc8428da47b6ace2da8b
Monica Bettencourt <monicabettencourt7@gmail.com>
2013-03-18 10:09:52 -0700

    Getting Started

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ctoforum::Application.config.secret_token = '508bf595aa762a1421844608e0d6d8540626b8460d0df4c7109c4ed40587735a95d9cc5d3273e835a66c2479f49de87a309284a67f0fa8525899e3fdb156235c'
